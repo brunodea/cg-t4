725ca228a2a5526513f64258be71d36ec768c2f1
brunodea <brunodea@inf.ufsm.br>
2011-07-09 00:31:54 -0300

    Classe Cylinder adicionada.

---
+++ include/model/Cylinder.hpp
@@ -0,0 +1,13 @@
+#ifndef _BRUNODEA_CYLINDER_HPP_
+#define _BRUNODEA_CYLINDER_HPP_
+
+#include "math/matrix_functions.hpp"
+
+namespace model
+{
+    class Cylinder
+    {
+    }; //end of class Cylinder.
+} //end of namespace model.
+
+#endif
